fbdb46e32204b74b857cf60acbe66f99dca365b1
anwiednn <an.wiednn@gmail.com>
2021-08-14 14:26:57 +1200

    Add detail view model and update service

--- angular-app/src/app/tasks/task.service.ts
+++ angular-app/src/app/tasks/task.service.ts
@@ -1,7 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+import { TaskDetailViewModel } from './detail/task-detail-view-model';
+import { map } from "rxjs/operators";
 
 @Injectable()
 export class TaskService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {
+    this.httpClient.options(
+      environment.apiBaseUrl, {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+  }
+
+  public getTaskDetail(id: number): Observable<TaskDetailViewModel> {
+    return this.httpClient
+      .get(`${environment.apiBaseUrl}/${id}`)
+      //.pipe(map(response => response as TaskDetailViewModel));
+  }
 }
