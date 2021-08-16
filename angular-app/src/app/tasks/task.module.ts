42905a0e6ea2904b945112b410b87cfac41ef831
anwiednn <an.wiednn@gmail.com>
2021-08-16 22:45:49 +1200

    Update modules. Add path (RouterModule)

--- angular-app/src/app/tasks/task.module.ts
+++ angular-app/src/app/tasks/task.module.ts
@@ -4,8 +4,7 @@ import { TaskService } from './task.service';
 import { TaskCreateComponent } from './create/task-create.component';
 import { TaskDetailComponent } from './detail/task-detail.component';
 import { TaskIndexComponent } from './index/task-index.component';
-
-
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -14,7 +13,18 @@ import { TaskIndexComponent } from './index/task-index.component';
     TaskIndexComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild([{
+        path: "",
+        component: TaskIndexComponent,
+        children: [{
+          path: "create",
+          component: TaskCreateComponent
+        }, {
+          path: "detail/:id",
+          component: TaskDetailComponent
+        }]
+    }])
   ],
   providers: [
     TaskService
--- angular-app/src/app/users/user.module.ts
+++ angular-app/src/app/users/user.module.ts
@@ -5,6 +5,7 @@ import { UserCreateComponent } from './create/user-create.component';
 import { UserDetailComponent } from './detail/user-detail.component';
 import { UserIndexComponent } from './index/user-index.component';
 import { UserEmailAvailableDirectiveDirective } from './shared/user-email-available-directive.directive';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,18 @@ import { UserEmailAvailableDirectiveDirective } from './shared/user-email-availa
     UserEmailAvailableDirectiveDirective
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild([{
+        path: "",
+        component: UserIndexComponent,
+        children: [{
+          path: "create",
+          component: UserCreateComponent
+        }, {
+          path: "detail/:id",
+          component: UserDetailComponent
+        }]
+    }])
   ],
   providers: [
     UserService
