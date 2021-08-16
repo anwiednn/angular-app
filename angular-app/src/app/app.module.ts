ce4dbd6c483f970c95c16cbfab02ca75f81c1750
anwiednn <an.wiednn@gmail.com>
2021-08-16 23:04:24 +1200

    Basic app layout

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -12,6 +16,10 @@ import { RouterModule } from '@angular/router';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    MatListModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatSidenavModule,
     RouterModule.forRoot([{
         path: "tasks",
         loadChildren: () => import("./tasks/task.module").then(m => m.TaskModule)
